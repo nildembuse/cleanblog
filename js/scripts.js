ebb5b2ae504b0ffdc918cf40a86e899d7602d627
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-08-27 15:21:16 +0300

    css, html ve js duzenlendi

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,58 @@
+const urlPrefix = 'https://jsonplaceholder.org/';
+const requestPostsUrl = urlPrefix + 'posts';
+const btn = document.querySelector('.olderPost');
+let posts = [];
+    
 
+function renderPosts() {
+    for (const post of posts) {
+        console.log(post.id);
+
+    }
+
+}
+
+function getFirstSentence(content) {
+    const sentences = content.split('.');
+    if (sentences.length > 0) {
+        return sentences[0] + '.';
+    }
+    return content;
+}
+
+
+const colmd10 = document.querySelector('.posts .row .col-md-10');
+
+
+function render() {
+    for (let i = 0; i < 5; i++) {
+        const currPost = posts[i];
+        const firstSentence = getFirstSentence(currPost.content);
+        colmd10.innerHTML += `
+            <div class="post-preview">
+                <a href="post.html">
+                    <h2 class="post-title">${currPost.title}</h2>
+                    <h3 class="post-subtitle">${firstSentence}</h3>
+                </a>
+                <p class="post-meta">
+                    " Posted by "
+                    <a href="#!">Start Bootstrap</a>
+                    " on ${currPost.publishedAt}"
+                </p>
+            </div>
+            <hr>
+        `;
+    }
+    bindPostsClicks();
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
+        if (currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
@@ -24,6 +70,3 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
-
-
-
